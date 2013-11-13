e9113fbfa8074a9814f9228d2ff8c7fd4aabc279
dantheus <dantheus@web.de>
2013-11-13 19:59:16 +0100

    GPS-Trace Extraction

--- GraphReconstruction/src/preprocessing/GPSMetricSpace.java
+++ GraphReconstruction/src/preprocessing/GPSMetricSpace.java
@@ -2,6 +2,7 @@ package preprocessing;
 
 import java.awt.geom.Point2D;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 
 import main.MetricSpace;
@@ -15,7 +16,7 @@ import main.MetricSpace;
  * The distance method is based on the shortest path metric of
  * a neighbourhood graph on the set of points.
  * 
- * @author Daniel Theus
+ * 
  */
 public class GPSMetricSpace extends HashSet<Point2D> implements MetricSpace<Point2D> {
 
@@ -44,9 +45,99 @@ public class GPSMetricSpace extends HashSet<Point2D> implements MetricSpace<Poin
 	 * @param filename the file containing the coordinates
 	 * @return the set of coordinates
 	 */
+	
+	
+	
+	
 	private Set<Point2D> parse(String filename) {
-		// TODO implement parse() method
-		return null;
+		
+		
+		String path= filename;
+		
+		Set<Point2D> koordinates = new Set<Point2D>();
+		
+		// Read the GPS-Trace File
+		RandomAccessFile file1 = new RandomAccessFile(path, "rw" );
+			
+		
+		byte[] data = new byte[(int) (file1.length())] ;
+		int a = file1.read(data); 
+		file1.close();
+		
+		String s1 = new String(dic);	//switch the byte array into a string
+				
+		// switch to singalSymbols (in front of the koordinates)
+		// and replace other Symbols
+			
+		String s2  = s1.replaceAll( "lon=","!"); 
+		s2  = s2.replaceAll( "lat=","#");
+		s2 = s2.replace("\"","");
+		s2 = s2.replace("?","");
+		s2 = s2.replace("<", " ");
+		s2 = s2.replace(">", "");
+		s2 = s2.replace("/", "");
+		s2 = s2.replace(":", "");
+		s2 = s2.replace("=", "");
+			
+		// delete all literals
+				
+		for(int i=65;i<91;i++){
+			String x = Character.toString((char)i);
+			String y= Character.toString((char)(i+32));
+			s2=s2.replace(x,"");
+			s2=s2.replace(y,"");
+		}
+		
+		// String of all koordinates
+		s2 = s2.replace("-","/");
+				
+		
+		
+		/*
+		 * the tokenizer takes all out of the string s2
+		 * ,replaces the last singalSymbols and turns
+		 * the koordinates into Integers
+		 * 
+		 */
+		StringTokenizer tokenizer = new StringTokenizer(s2);
+		LinkedList<Integer> koordX = new LinkedList<Integer>();
+		LinkedList<Integer> koordY = new LinkedList<Integer>();
+				
+		while ( tokenizer.hasMoreTokens() ) 
+		{
+					
+		String write2=(String)tokenizer.nextToken();
+					
+			if(write2.contains("!")){
+				//koordX.add(Integer.parseInt(write2.replace("!", "")+"\n"));
+				write2=write2.replace("!", "");
+				write2=write2.replace(".", "");
+				koordX.add(Integer.parseInt(write2));
+			}
+			if(write2.contains("#")){
+				//koordY.add(Integer.parseInt(write2.replace("#", "")+"\n"));
+				
+				write2=write2.replace("#", "");
+				write2=write2.replace(".", "");
+				koordY.add(Integer.parseInt(write2));
+			}
+		}
+			
+		
+		// loop to place them into the set
+		
+		for (int i=0;i<koordY.size();i++){
+			Point2D point = new Point2D();
+			point.setLocation(koordX.poll(), koordY.poll());		
+			koordinates.add(point);	
+		}
+		
+			
+		}		
+		
+		// returns the set
+		
+		return koordinates;
 	}
 
 }
