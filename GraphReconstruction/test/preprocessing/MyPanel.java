825d092259a4e4fb01a55fb77deda0bf24dbd240
hongliang <andreasjhl@hotmail.com>
2013-11-14 15:03:56 +0100

    myPannel , MainApplet  and Model  update, just for test

---
+++ GraphReconstruction/test/preprocessing/MyPanel.java
@@ -0,0 +1,16 @@
+package preprocessing;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JApplet;
+import javax.swing.JPanel;
+
+import model.Model;
+
+public class MyPanel extends JPanel {
+
+	Model model;
+
+
+}
