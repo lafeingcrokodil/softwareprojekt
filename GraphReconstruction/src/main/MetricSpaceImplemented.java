ac6f08af8e0f34d9a05bb9085983b5623197367f
buriti <buriti@buriti.(none)>
2013-11-14 18:53:53 +0100

    Draft of implementation of MetricSpace

--- GraphReconstruction/src/main/MetricSpaceImplemented.java
+++ GraphReconstruction/src/main/MetricSpaceImplemented.java
@@ -1,8 +1,14 @@
 package main;
 
-public class MetricSpaceImplemented implements MetricSpace{
+import java.util.HashSet;
+	/*
+	 * @param <P> the type of points in the metric space
+	 */
+public class MetricSpaceImplemented<P> extends HashSet<E> implements MetricSpace<P>{
+	
 	public MetricSpace<P> pointsInRadius(P p, double r){
-		
+		//MetricSpace<P> space;
+		//return space;
 		/* 
 		 * TODO: returns MetricSpace composed of points within radius r
 		 */
@@ -14,10 +20,16 @@ public class MetricSpaceImplemented implements MetricSpace{
 		 */
 	}
 	
-	public void labelInRadius(P p, double r, MetricSpace<P> space){
+	public void labelInRadius(P p, double r){
 		/*
-		 * TODO: MetricSpace as input parameter?
+		 * Label 1 = Preliminary Branch
+		 * Label 2 = Edge
+		 * Label 3 = Branch
 		 */
+		for(P point : pointsInRadius(p, r)){
+			labelAs(point, 3);
+		}
+		
 	}
 	
 	public MetricSpace<P> getLabeledAs(int label){
