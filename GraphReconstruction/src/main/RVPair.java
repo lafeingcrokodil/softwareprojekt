6d2dc360d43c13a5fddb871b9f0e24eed227cb9b
mahmoudkassem <mahmoud_k@mail.com>
2013-11-16 14:09:48 +0100

    A helper class

--- GraphReconstruction/src/main/RVPair.java
+++ GraphReconstruction/src/main/RVPair.java
@@ -2,15 +2,16 @@ package main;
 
 
 public class RVPair <E>{
-    private <E> representative;
+    private E representative;
     private LinkedList<E> neighbours;
 
-    public RVPair(<E> representative, LinkedList<E> neighbours){
+    public RVPair(E representative, LinkedList<E> neighbours)
+    {
         this.rep   = representative;
         this.neighbours = neighbours;
     }
 
-    public <E> get_representative() { return representative; }
+    public E get_representative()   { return representative; }
     public LinkedList<E> get_neighbours() { return neighbours; }
-    public void add_neighbour(<E> point) { neighbours.addFirst(point); }
+    public void add_neighbour(E point) { neighbours.addFirst(point); }
 }
