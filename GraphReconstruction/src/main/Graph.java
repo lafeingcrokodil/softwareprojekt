9c726ab8bb99c17f290b1f7cfc10d8edd20f9947
Moritzw <devils@nurfuerspam.de>
2013-11-23 19:19:03 +0100

    updated graph

--- GraphReconstruction/src/main/Graph.java
+++ GraphReconstruction/src/main/Graph.java
@@ -1,9 +1,13 @@
 package main;
+import java.util.List;
 
-import java.util.Set;
+/* 
+ * Main graph Interface. @visualization: if more or diffrent methods requested
+ * add them here.
+ */
 
 /**
- * A graph. Supports iteration over the set of nodes.
+ * A graph. Supports iteration over the nodes.
  *
  * @param <V> the type of nodes in this graph
  */
@@ -13,8 +17,23 @@ public interface Graph<V> extends Iterable<V> {
 	 * Returns all nodes that are directly connected to the specified node by an edge.
 	 * 
 	 * @param node the node in question
-	 * @return all neighbours of the node in question
+	 * @return all neighbours of the node in question, null if no neighbours exist
 	 */
-	Set<V> getNeighbours(V node);
-
+	List<V> getNeighbours(V node);
+	/**
+	 * Returns the length of the edge between node1 and node2.
+	 * 
+	 * @param node1, node2 the nodes in question
+	 * @return double value of the distance and -1 if no edge exist between node1 and node2
+	 */
+	double getDistance(V node1, V node2);
+	/**
+	 * Returns all nodes in the graph.
+	 * 
+	 * @param none
+	 * @return all nodes
+	 */
+	List<V> getAllNodes();
+	
+	
 }
